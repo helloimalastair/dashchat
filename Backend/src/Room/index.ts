26eaa20b841f8025c058b0eb5a79b8a1080f1014
Isaac McFadyen <6243993+mcfadyeni@users.noreply.github.com>
2022-06-06 18:04:20 -0400

    Updated backend.

--- Backend/src/Room/index.ts
+++ Backend/src/Room/index.ts
@@ -20,7 +20,7 @@ export class Room {
 
   constructor(state: DurableObjectState, env: Environment) {
     this.ids = {
-      do: state.id.toString()
+      do: state.id.toString(),
     };
     this.storage = state.storage;
     this.blockConcurrencyWhile = state.blockConcurrencyWhile;
@@ -28,18 +28,21 @@ export class Room {
     this.maxOccupants = Number(env.MaxOccupants);
     this.blockConcurrencyWhile(async () => {
       const status = await this.storage.get<RoomStatus>("status");
-      if(status) this.status = status;
+      if (status) this.status = status;
     });
   }
 
   // Private method to connect a user to the room.
-  private async connect(req: Request) : Promise<Response> {
-    if(this.status !== "open") return generateErrorResponse(`Room is ${this.status}`, 503);
+  private async connect(req: Request): Promise<Response> {
+    if (this.status !== "open")
+      return generateErrorResponse(`Room is ${this.status}`, 503);
     const uname = req.headers.get("uname"),
       isOwner = Boolean(req.headers.get("isOwner"));
-    if(!uname) return usernameDoesNotExist();
-    if(isOwner && this.connections.find(e => e.isOwner)) return generateErrorResponse("Owner already connected");
-    if(this.connections.find(e => e.uname === uname)) return generateErrorResponse("Username already connected");
+    if (!uname) return usernameDoesNotExist();
+    if (isOwner && this.connections.find((e) => e.isOwner))
+      return generateErrorResponse("Owner already connected");
+    if (this.connections.find((e) => e.uname === uname))
+      return generateErrorResponse("Username already connected");
     const [client, server] = Object.values(new WebSocketPair());
     server.accept();
     const connection: Connection = {
@@ -48,34 +51,44 @@ export class Room {
       location: req.cf?.country,
       ws: server,
     };
-    server.addEventListener("message", async event => handleMessage(this, connection, event));
+    server.addEventListener("message", async (event) =>
+      handleMessage(this, connection, event)
+    );
     server.addEventListener("close", () => handleDisconnect(this, connection));
-    if(connection.isOwner) this.owner = connection;
+    if (connection.isOwner) this.owner = connection;
     this.connections.push(connection);
-    if(this.connections.length >= this.maxOccupants) {
+    if (this.connections.length >= this.maxOccupants) {
       this.status = "at capacity";
-      if(!this.ids.external) return generateErrorResponse("Room has not been initiated", 500);
-      await this.env.KV.put(`${this.env.KVPrefix}-${this.ids.external}`, JSON.stringify({
-        status: this.status,
-        ownerId: this.ids.external,
-        doId: this.ids.do
-      }));
+      if (!this.ids.external)
+        return generateErrorResponse("Room has not been initiated", 500);
+      await this.env.KV.put(
+        `${this.env.KVPrefix}-${this.ids.external}`,
+        JSON.stringify({
+          status: this.status,
+          ownerId: this.ids.external,
+          doId: this.ids.do,
+        })
+      );
     }
-    return new Response("Upgrade in progress", {status: 101, webSocket: client});
+    return new Response("Upgrade in progress", {
+      status: 101,
+      webSocket: client,
+    });
   }
 
-  async fetch(req: Request) : Promise<Response> {
+  async fetch(req: Request): Promise<Response> {
     const path = new URL(req.url).pathname;
-    switch(path) {
+    switch (path) {
       case "/startup":
-        const data = await req.json() as StartupData;
+        console.log("Received input!");
+        const data = (await req.json()) as StartupData;
         this.ids.external = data.external;
         this.ids.owner = data.owner;
         this.ids.name = data.name;
         this.isGDPR = data.isGDPR;
         this.videoLength = data.videoLength;
-        if(data.maxOccupants) this.maxOccupants = data.maxOccupants;
-        await this.storage.setAlarm(Date.now() + 60000);
+        if (data.maxOccupants) this.maxOccupants = data.maxOccupants;
+        await this.storage.setAlarm(Date.now() + 25000);
         return new Response("OK");
       case "/ready":
         this.videoReady = true;
@@ -83,21 +96,26 @@ export class Room {
       case "/delete":
         await closeAll(this);
         await this.storage.deleteAll();
-        return new Response("Room deleted", {status: 200});
+        return new Response("Room deleted", { status: 200 });
       case "/connect":
-        if(!this.videoReady) return generateErrorResponse("Video has not finished processing");
+        if (!this.videoReady)
+          return generateErrorResponse("Video has not finished processing");
         return this.connect(req);
     }
-    return new Response("Invalid Internal URL", {status: 404});
+    return new Response("Invalid Internal URL", { status: 404 });
   }
 
   async alarm() {
-    if(this.status === "ended") {
-      await this.env.KV.put(`${this.env.KVPrefix}-${this.ids.external}-log`, JSON.stringify(this.log), {metadata: {owner: this.ids.owner}});
+    if (this.status === "ended") {
+      await this.env.KV.put(
+        `${this.env.KVPrefix}-${this.ids.external}-log`,
+        JSON.stringify(this.log),
+        { metadata: { owner: this.ids.owner } }
+      );
       return await this.storage.deleteAll();
     }
     await this.storage.put("log", this.log);
     analyticsUpdate(this);
-    await this.storage.setAlarm(Date.now() + 60000);
+    await this.storage.setAlarm(Date.now() + 25000);
   }
 }
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,16 +12,15 @@ app.use("*", cookie());
 app.use("*", async (c, next) => {
   await next();
   const response = new Response(c.res.body, c.res);
-  response.headers.set("Access-Control-Allow-Origin", "http://localhost");
-  response.headers.set("Access-Control-Allow-Headers", "*");
-  response.headers.set("Access-Control-Allow-Methods", "*");
-  response.headers.set("Access-Control-Allow-Credentials", "*");
+  response.headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Cookie");
+  response.headers.set("Access-Control-Allow-Credentials", "true");
   return response;
 });
 
 // Options Support
-app.options("*", () => new Response("", {status: 204}));
-app.get("/hello", c => c.text("Hello World!"));
+app.options("*", () => new Response("", { status: 204 }));
+app.get("/hello", (c) => c.text("Hello World!"));
 
 // Add Username
 app.post("/identify", identifyUser);
